68d7f4bf39a2ca12fb2b9ecbe1d28513a632b30d
Tom Mertens <tom.mertens@helmholtz-berlin.de>
2020-07-13 22:18:46 +0200

    added pymodule to setup

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name = 'intake-acclat',
-    version = '0.0.1',
-    packages = find_packages(),
-    entry_points = {
-        'intake.drivers': [
-            'local-acclat = intake_acclat.intake_acclat:LocalAccLat',
-            'acclatsource = intake_acclat.source.AccLat:AccLatSource',      
+    name="intake-acclat",
+    version="0.0.1",
+    py_modules=["intake_acclat"],
+    packages=find_packages(),
+    entry_points={
+        "intake.drivers": [
+            "local-acclat = intake_acclat.intake_acclat:LocalAccLat",
+            "acclatsource = intake_acclat.source.AccLat:AccLatSource",
         ]
     },
-    include_package_data = True,
-    install_requires = ['intake'],
-    zip_safe=False
+    include_package_data=True,
+    install_requires=["intake"],
+    zip_safe=False,
 )
